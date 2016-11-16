12881372dfac3f99811c50d28a2600575e26eeb1
Darryl Hein <dhein@xmmedia.com>
2016-11-16 15:36:33 -0700

    correcting namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace XM\FilterBundle\DependencyInjection;
+namespace XM\AssetVersionBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
